ee1f7d2e207da5088ca51e3ece955f8a3dc1cc98
TimLucca <tim.lucca88@gmail.com>
2019-08-04 14:04:30 -0600

    Updates

---
+++ precache-manifest.f3605e57ecc80fe4b654bfd4dd2a8554.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b516b45b9c7ed5b5521a9e25e509718e",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "afb3ed669293d07c0136",
+    "url": "/todolist/static/css/main.6c6ea511.chunk.css"
+  },
+  {
+    "revision": "f0ca87bc9223f94baec2",
+    "url": "/todolist/static/js/2.d056c62e.chunk.js"
+  },
+  {
+    "revision": "afb3ed669293d07c0136",
+    "url": "/todolist/static/js/main.bd2a9e1e.chunk.js"
+  },
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  }
+]);
