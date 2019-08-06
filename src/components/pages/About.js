1a9f2a04ce1199d6e6d22ee265e58424090abdda
TimLucca <tim.lucca88@gmail.com>
2019-08-06 11:22:42 -0600

    added link to source code

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,6 +6,7 @@ export default function About() {
       <h1>About</h1>
       <p>Todo app built with React. 
         Crash course from <a href='https://www.youtube.com/watch?v=sBws8MSXN7A&t=491s'>Traversy Media</a></p>
+        <p><a href='https://github.com/TimLucca/todolist'>Source Code</a></p>
     </React.Fragment>
   )
 }
